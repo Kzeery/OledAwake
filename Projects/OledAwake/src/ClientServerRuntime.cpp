510bb4de58627bf339b709d018849c51091984d5
Yousif Kzeer <kzeery@mcmaster.ca>
2022-01-26 20:08:15 -0500

    Version 2.3.1 - Bug fixing and using TCP/IP instead of named pipes for network communication between computers

--- Projects/OledAwake/src/ClientServerRuntime.cpp
+++ Projects/OledAwake/src/ClientServerRuntime.cpp
@@ -4,16 +4,29 @@
 #include "PipeCommunication.h"
 typedef ULONG(WINAPI* pGetAdaptersInfo)(PIP_ADAPTER_INFO, PULONG);
 std::unique_ptr<Runtime> ClientServerRuntime::Instance_ = nullptr;
+DWORD WINAPI sendMessageToServer(_In_ LPVOID);
 
 ClientServerRuntime::~ClientServerRuntime()
 {
-    RemoteRequest req = RemoteRequest::QUIT;
-    MonitorState res = MonitorState::MONITOR_OFF;
-    DWORD bytesRead = 0;
-    CallNamedPipe(REMOTE_PIPE_NAME("."), &req, REMOTE_IN_BUFFER_SIZE, &res, REMOTE_OUT_BUFFER_SIZE, &bytesRead, NMPWAIT_NOWAIT);
-    if (PipeServerThread_ != nullptr && PipeServerThread_->joinable())
+    sendMessageToServerTimeout(RemoteRequest::QUIT, nullptr, true, 200);
+    if (ServerThread_ != nullptr && ServerThread_->joinable())
     {
-        PipeServerThread_->join();
+        ServerThread_->join();
+    }
+}
+
+void ClientServerRuntime::sendMessageToServerTimeout(RemoteRequest req, MonitorState* res, bool self, int maxTimeout) const
+{
+    SendServerInfoStruct serverinfo;
+    serverinfo.ip = self ? LocalIP_ : OtherIP_;
+    serverinfo.req = req;
+    serverinfo.res = res;
+
+    HANDLE hThread = CreateThread(NULL, 0, sendMessageToServer, &serverinfo, 0, NULL);
+    if (hThread)
+    {
+        WaitForSingleObject(hThread, maxTimeout);
+        CloseHandle(hThread);
     }
 }
 
@@ -38,11 +51,11 @@ bool ClientServerRuntime::init()
         return false;
     }
 
-    PipeServerThread_ = std::unique_ptr<std::thread>(new std::thread(&ClientServerRuntime::initServer, this));
+    ServerThread_ = std::unique_ptr<std::thread>(new std::thread(&ClientServerRuntime::initServer, this));
     if (WaitForSingleObject(serverRunningEvent, 5000) == WAIT_TIMEOUT)
     {
-        PipeServerThread_->join();
-        PipeServerThread_.reset(nullptr);
+        ServerThread_->join();
+        ServerThread_.reset(nullptr);
         Utilities::setLastError("Server running event timed out!");
         CloseHandle(serverRunningEvent);
         return false;
@@ -54,10 +67,8 @@ bool ClientServerRuntime::init()
 
 MonitorState ClientServerRuntime::getOtherMonitorState() const
 {
-    RemoteRequest req = RemoteRequest::GET_MONITOR_STATE;
-    MonitorState res = MonitorState::MONITOR_OFF;
-    DWORD bytesRead = 0;
-    CallNamedPipeW(RemotePipeName_.c_str(), &req, REMOTE_IN_BUFFER_SIZE, &res, REMOTE_OUT_BUFFER_SIZE, &bytesRead, NMPWAIT_NOWAIT);
+    MonitorState res = MonitorState::UNKNOWN;
+    sendMessageToServerTimeout(RemoteRequest::GET_MONITOR_STATE, &res, false, 200);
     return res;
 }
 
@@ -73,7 +84,7 @@ void ClientServerRuntime::setCurrentMonitorState(MonitorState state)
 
 bool ClientServerRuntime::ensureServerEnvironment()
 {
-
+    if (!LocalIP_.empty()) return true;
     HMODULE hDLL = LoadLibrary(L"Iphlpapi.dll");
     if (!hDLL)
     {
@@ -125,131 +136,128 @@ bool ClientServerRuntime::ensureServerEnvironment()
     }
     if (LocalIP_ == IP_ADDRESS_A)
     {
-        RemotePipeName_ = REMOTE_PIPE_NAME(IP_ADDRESS_B);
+        OtherIP_ = IP_ADDRESS_B;
         IsDeviceManager_ = true;
     }
     else
     {
-        RemotePipeName_ = REMOTE_PIPE_NAME(IP_ADDRESS_A);
+        OtherIP_ = IP_ADDRESS_A;
         IsDeviceManager_ = false;
     }
     return true;
 }
+
 bool ClientServerRuntime::initServer()
 {
-    bool firstRun = true;
-    bool signalEvent = false;
-    HANDLE hPipe = INVALID_HANDLE_VALUE;
+    WSADATA wsaData;
+
+    int iResult;
+    struct addrinfo* result = NULL, * ptr = NULL, hints;
+
+    // Initialize Winsock
+    iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (iResult != 0) {
+        return false;
+    }
+
+    ZeroMemory(&hints, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+    hints.ai_flags = AI_PASSIVE;
+
+    // Resolve the local address and port to be used by the server
+    iResult = getaddrinfo(LocalIP_.c_str(), SERVICE_PORT, &hints, &result);
+    if (iResult != 0) {
+        WSACleanup();
+        return false;
+    }
+
+    SOCKET ListenSocket = INVALID_SOCKET;
+
+    ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+    if (ListenSocket == INVALID_SOCKET) {
+        freeaddrinfo(result);
+        WSACleanup();
+        return false;
+    }
+
+    iResult = bind(ListenSocket, result->ai_addr, (int)result->ai_addrlen);
+    if (iResult == SOCKET_ERROR) {
+        freeaddrinfo(result);
+        closesocket(ListenSocket);
+        WSACleanup();
+        return false;
+    }
+
+    if (listen(ListenSocket, SOMAXCONN) == SOCKET_ERROR) {
+        freeaddrinfo(result);
+        closesocket(ListenSocket);
+        WSACleanup();
+        return false;
+    }
+
+    freeaddrinfo(result);
+    SOCKET ClientSocket;
+    ClientSocket = INVALID_SOCKET;
+
+    HANDLE serverRunningEvent = OpenEvent(EVENT_ALL_ACCESS, FALSE, L"ServerRunning");
+    if (serverRunningEvent == NULL)
+    {
+        closesocket(ListenSocket);
+        WSACleanup();
+        return false;
+    }
+
+    SetEvent(serverRunningEvent);
+    CloseHandle(serverRunningEvent);
     while (true)
     {
-
-        SECURITY_ATTRIBUTES attrib;
-        std::unique_ptr<SECURITY_DESCRIPTOR> desc(new SECURITY_DESCRIPTOR{ 0 });
-        InitializeSecurityDescriptor(desc.get(), SECURITY_DESCRIPTOR_REVISION);
-        SetSecurityDescriptorDacl(desc.get(), TRUE, (PACL)NULL, FALSE);
-
-        attrib.nLength = sizeof(SECURITY_ATTRIBUTES);
-        attrib.bInheritHandle = TRUE;
-        attrib.lpSecurityDescriptor = desc.get();
-
-        hPipe = CreateNamedPipe(REMOTE_PIPE_NAME("."), PIPE_ACCESS_DUPLEX, PIPE_TYPE_MESSAGE | PIPE_WAIT, PIPE_UNLIMITED_INSTANCES, REMOTE_OUT_BUFFER_SIZE, REMOTE_IN_BUFFER_SIZE, NULL, &attrib);
-        if (hPipe == INVALID_HANDLE_VALUE)
-        {
-            signalEvent = true;
+        ClientSocket = accept(ListenSocket, NULL, NULL);
+        if (ClientSocket == INVALID_SOCKET) {
             break;
         }
 
-        if (firstRun)
-        {
-            firstRun = false;
-            HANDLE serverRunningEvent = OpenEvent(EVENT_ALL_ACCESS, FALSE, L"ServerRunning");
-            if (serverRunningEvent == NULL)
+        RemoteRequest clientRequest = RemoteRequest::REQUEST_UNKNOWN;
+        MonitorState serverResponse = MonitorState::UNKNOWN;
+        int iSendResult;
+        bool needToExit = false;
+        iResult = recv(ClientSocket, (char*)&clientRequest, REMOTE_IN_BUFFER_SIZE, 0);
+        if (iResult > 0) {
+
+            switch (clientRequest)
             {
-                signalEvent = true;
+            case RemoteRequest::GET_MONITOR_STATE:
+                serverResponse = State_;
+                break;
+            case RemoteRequest::QUIT:
+                closesocket(ClientSocket);
+                needToExit = true;
+                break;
+            default:
                 break;
             }
-            SetEvent(serverRunningEvent);
-            CloseHandle(serverRunningEvent);
-        }
 
-        BOOL fConnected = ConnectNamedPipe(hPipe, NULL) ?
-            TRUE : (GetLastError() == ERROR_PIPE_CONNECTED);
-        if (fConnected)
-        {
-            if (!HandleConnection(hPipe))
+            if (needToExit)
                 break;
+
+            iSendResult = send(ClientSocket, (char*)&serverResponse, REMOTE_OUT_BUFFER_SIZE, 0);
+            closesocket(ClientSocket);
+            if (iSendResult == SOCKET_ERROR) {
+                break;
+            }
         }
         else
-            CloseHandle(hPipe);
+            closesocket(ClientSocket);
     }
+    
+    closesocket(ListenSocket);
+    WSACleanup();
 
-    if (signalEvent)
-    {
-        HANDLE serverExitedEvent = OpenEvent(EVENT_ALL_ACCESS, FALSE, Utilities::eventNames[SERVER_EXITED_EVENT_INDEX]);
-        if (!serverExitedEvent) return false;
-        SetEvent(serverExitedEvent);
-        CloseHandle(serverExitedEvent);
-    }
-    return true;
-}
-
-void WINAPI cleanup(HANDLE& hPipe)
-{
-    DisconnectNamedPipe(hPipe);
-    CloseHandle(hPipe);
-}
-
-bool ClientServerRuntime::HandleConnection(HANDLE& hPipe)
-{
-    std::unique_ptr<RemoteRequest> pchRequest(new RemoteRequest);
-    std::unique_ptr<MonitorState> pchReply(new MonitorState);
-
-    DWORD cbBytesRead = 0, cbReplyBytes = 0, cbWritten = 0;
-    BOOL fSuccess = FALSE;
-
-    fSuccess = ReadFile(
-        hPipe,        // handle to pipe 
-        pchRequest.get(),    // buffer to receive data 
-        REMOTE_IN_BUFFER_SIZE, // size of buffer 
-        &cbBytesRead, // number of bytes read 
-        NULL);        // not overlapped I/O 
-
-    if (!fSuccess || cbBytesRead == 0)
-    {
-        cleanup(hPipe);
-        return true;
-    }
-
-    switch (*pchRequest)
-    {
-    case RemoteRequest::GET_MONITOR_STATE:
-        *pchReply = RuntimeManager::getClientServerRuntime()->getCurrentMonitorState();
-        break;
-    default:
-        cleanup(hPipe);
-        return false;
-    }
-    // Process the incoming message.
-
-    // Write the reply to the pipe. 
-    fSuccess = WriteFile(
-        hPipe,        // handle to pipe 
-        pchReply.get(),     // buffer to write from 
-        REMOTE_OUT_BUFFER_SIZE, // number of bytes to write 
-        &cbWritten,   // number of bytes written 
-        NULL);        // not overlapped I/O 
-
-    if (!fSuccess || REMOTE_OUT_BUFFER_SIZE != cbWritten)
-    {
-        cleanup(hPipe);
-        return true;
-    }
-
-    // Flush the pipe to allow the client to read the pipe's contents 
-    // before disconnecting. Then disconnect the pipe, and close the 
-    // handle to this pipe instance. 
-    FlushFileBuffers(hPipe);
-    cleanup(hPipe);
+    HANDLE serverExitedEvent = OpenEvent(EVENT_ALL_ACCESS, FALSE, Utilities::eventNames[SERVER_EXITED_EVENT_INDEX]);
+    if (!serverExitedEvent) return false;
+    SetEvent(serverExitedEvent);
+    CloseHandle(serverExitedEvent);
     return true;
 }
 
@@ -326,4 +334,86 @@ bool ClientServerRuntime::postWindowsTurnOffDisplayMessage() const
         return false;
     }
     return true;
+}
+
+
+
+DWORD WINAPI sendMessageToServer(_In_ LPVOID sendInfo)
+{
+    SendServerInfoStruct* info = (SendServerInfoStruct*)sendInfo;
+    WSADATA wsaData;
+    int iResult;
+
+    // Initialize Winsock
+    iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (iResult != 0) {
+        return 1;
+    }
+
+    struct addrinfo* result = NULL,
+        * ptr = NULL,
+        hints;
+
+    ZeroMemory(&hints, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+
+
+    iResult = getaddrinfo(info->ip.c_str(), SERVICE_PORT, &hints, &result);
+    if (iResult != 0) {
+        WSACleanup();
+        return 1;
+    }
+
+    SOCKET ConnectSocket = INVALID_SOCKET;
+
+    ptr = result;
+
+    // Create a SOCKET for connecting to server
+    ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype,
+        ptr->ai_protocol);
+
+    if (ConnectSocket == INVALID_SOCKET) {
+        freeaddrinfo(result);
+        WSACleanup();
+        return 1;
+    }
+
+    iResult = connect(ConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
+    if (iResult == SOCKET_ERROR) {
+        closesocket(ConnectSocket);
+        ConnectSocket = INVALID_SOCKET;
+    }
+
+    freeaddrinfo(result);
+
+    if (ConnectSocket == INVALID_SOCKET) {
+        WSACleanup();
+        return 1;
+    }
+
+    // Send an initial buffer
+    iResult = send(ConnectSocket, (char*)&info->req, REMOTE_IN_BUFFER_SIZE, 0);
+    if (iResult == SOCKET_ERROR) {
+        closesocket(ConnectSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    // shutdown the connection for sending since no more data will be sent
+    // the client can still use the ConnectSocket for receiving data
+    iResult = shutdown(ConnectSocket, SD_SEND);
+
+    if (iResult == SOCKET_ERROR) {
+        closesocket(ConnectSocket);
+        WSACleanup();
+        return 1;
+    }
+    if (info->req != RemoteRequest::QUIT)
+        iResult = recv(ConnectSocket, (char*)info->res, REMOTE_OUT_BUFFER_SIZE, 0);
+
+    closesocket(ConnectSocket);
+    WSACleanup();
+    return 0;
 }
